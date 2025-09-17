f662dc565587fb63643321e663f2fd2184d18c29
BeBeBecon <katsube0611@gmail.com>
2025-09-17 18:18:05 +0900

    Initial Commit

---
+++ AppleAiApp/AppleAiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleAiAppApp.swift
+//  AppleAiApp
+//
+//  Created by Taku.K on 2025/09/17.
+//
+
+import SwiftUI
+
+@main
+struct AppleAiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleAiAppTests/AppleAiAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleAiAppTests.swift
+//  AppleAiAppTests
+//
+//  Created by Taku.K on 2025/09/17.
+//
+
+import Testing
+@testable import AppleAiApp
+
+struct AppleAiAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
